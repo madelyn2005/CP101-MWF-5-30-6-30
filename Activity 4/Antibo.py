6f9cc722045f6c721143d54fbb0b8410292dde84
madelyn2005 <madelynantibo1@gmail.com>
2024-09-24 02:57:21 -0700

    Antibo.py

---
+++ Activity 4/Antibo.py
@@ -0,0 +1,15 @@
+# sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+# sampleText1a = sampleText1.format("Madelyn", "Badminton", "Yacult", "Mango")
+# print(sampleText1a)
+
+
+
+# sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+# sampleText2a = sampleText2.format("COD", "Madelyn", "Yacult", "Badminton", "Mango")
+# print(sampleText2a)
+
+
+
+# sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+# sampleText3a = sampleText3.format(food="Yacult", play="Badminton", name="Madelyn")
+# print(sampleText3a)
